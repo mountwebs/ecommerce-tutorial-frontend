f0743913ef68e7c7dbf394bf7c05855974e6895e
mountwebs <mountwebs@gmail.com>
2021-12-04 00:49:35 +0100

    completed section 2: Starting the frontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 import "./bootstrap.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1>Hei</h1>
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
